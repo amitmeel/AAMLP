647192f7ebb32ee073cd1b76330e2166486b553d
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2021-12-21 12:47:43 +0530

    greedy feature selection wrapper

--- feature_selection_wrapper.py
+++ feature_selection_wrapper.py
@@ -1,5 +1,6 @@
 """ This module is used to create a wrapper for the feature selection"""
 
+import pandas as pd
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import f_classif
 from sklearn.feature_selection import f_regression
@@ -7,8 +8,16 @@ from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectPercentile
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.datasets import make_classification
 
 # create a wrapper to select univariate features
+"""
+Univariate feature selection is nothing but a scoring of each feature
+against a given target. Mutual information, ANOVA F-test and chi2 are
+some of the most popular methods for univariate feature selection
+"""
 class UnivariateFeatureSelectionWrapper:
     """
     This class is used to create a wrapper for the feature selection for univariate features
@@ -139,6 +148,110 @@ class UnivariateFeatureSelction:
         self.fit(X, y)
         return self.transform(X)
 
+## greedy feature selection wrapper
+"""
+The simplest form of feature selection that uses a model for selection is known as
+greedy feature selection. In greedy feature selection, the first step is to choose a
+model. The second step is to select a loss/scoring function. And the third and final
+step is to iteratively evaluate each feature and add it to the list of “good” features if
+it improves loss/score
+This feature selection process
+will fit a given model each time it evaluates a feature. The computational cost
+associated with this kind of method is very high.
+"""
+class GreedyFeatureSelection:
+    """
+    A simple and custom class for greedy feature selection.
+    You will need to modify it quite a bit to make it suitable
+    for your dataset.
+    """
+    def evaluate_score(self, X, y):
+        """
+        This function evaluates model on data and returns
+        Area Under ROC Curve (AUC)
+        NOTE: We fit the data and calculate AUC on same data.
+        WE ARE OVERFITTING HERE.
+        But this is also a way to achieve greedy selection.
+        k-fold will take k times longer.
+        If you want to implement it in really correct way,
+        calculate OOF AUC and return mean AUC over k folds.
+        This requires only a few lines of change and has been
+        shown a few times in this book.
+        :param X: training data
+        :param y: targets
+        :return: overfitted area under the roc curve
+        """
+        # fit the logistic regression model,
+        # and calculate AUC on same data
+        # again: BEWARE
+        # you can choose any model that suits your data
+        model = linear_model.LogisticRegression()
+        model.fit(X, y)
+        predictions = model.predict_proba(X)[:, 1]
+        auc = metrics.roc_auc_score(y, predictions)
+
+        return auc
+    
+    def _feature_selection(self, X, y):
+        """
+        This function does the actual greedy selection
+        :param X: data, numpy array
+        :param y: targets, numpy array
+        :return: (best scores, best features)
+        """
+        # initialize good features list
+        # and best scores to keep track of both
+        good_features = []
+        best_scores = []
+        # calculate the number of features
+        num_features = X.shape[1]
+        # infinite loop
+        while True:
+            # initialize best feature and score of this loop
+            this_feature = None
+            best_score = 0
+            # loop over all features
+            for feature in range(num_features):
+                # if feature is already in good features,
+                # skip this for loop
+                if feature in good_features:
+                    continue
+                # selected features are all good features till now
+                # and current feature
+                selected_features = good_features + [feature]
+                # remove all other features from data
+                xtrain = X[:, selected_features]
+                # calculate the score, in our case, AUC
+                score = self.evaluate_score(xtrain, y)
+                # if score is greater than the best score
+                # of this loop, change best score and best feature
+                if score > best_score:
+                    this_feature = feature
+                    best_score = score
+                # if we have selected a feature, add it
+                # to the good feature list and update best scores list
+                if this_feature != None:
+                    good_features.append(this_feature)
+                    best_scores.append(best_score)
+                # if we didnt improve during the previous round,
+                # exit the while loop
+                if len(best_scores) > 2:
+                    if best_scores[-1] < best_scores[-2]:
+                        break
+        # return best scores and good features
+        # why do we remove the last data point?
+        return best_scores[:-1], good_features[:-1]
+
+    def __call__(self, X, y):
+        """
+        Call function will call the class on a set of arguments
+        """
+        # select features, return scores and selected indices
+        scores, features = self._feature_selection(X, y)
+        # transform data with selected features
+        return X[:, features], scores
+
+
 # if __name__=='__main__':
 #     ufs = UnivariateFeatureSelction(
 #         n_features=0.1,
@@ -148,3 +261,8 @@ class UnivariateFeatureSelction:
 #     ufs.fit(X, y)
 #     X_transformed = ufs.transform(X)
 
+# if __name__ == "__main__":
+#     # generate binary classification data
+#     X, y = make_classification(n_samples=1000, n_features=100)
+#     # transform data by greedy feature selection
+#     X_transformed, scores = GreedyFeatureSelection()(X, y)
